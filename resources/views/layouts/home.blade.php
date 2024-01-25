fc9682d49c5b6b4dc590997a86e2cf9dc0f98bac
khforever <eng.kholoudmohamed@gmail.com>
2024-01-25 03:02:15 +0200

    first commit

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,58 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   @include('includes.head')
+</head>
+
+<body>
+    
+        @include('includes.Spinner')
+
+
+        @include('includes.Navbar')
+
+
+
+
+        @include('includes.Carousel')
+
+
+
+        @include('includes.Facilities')
+
+
+        @include('includes.About')
+
+       
+        @include('includes.Action')
+
+
+
+       
+
+        @include('includes.Classes')
+
+
+
+        @include('includes.Appointment')
+
+       
+        @include('includes.Team')
+
+
+
+
+        @include('includes.Testimonial')
+ 
+
+        @include('includes.Footer')
+
+       
+    @include('includes.BackToTop')
+    @include('includes.js')
+</body>
+
+</html>
